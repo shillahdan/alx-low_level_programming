5af2eaa3e01a083079a852e60dbfba494944c389
shillahdan <awinosheila004@gmail.com>
2023-06-20 13:53:37 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,13 +28,13 @@ void times_table(void)
 			{
 				if (y != 0)
 				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
 				}
 				_putchar(z + '0');
-			}
-		}
-		_putchar('\n');
-	}
+			        }
+		                }
+		                _putchar('\n');
+	                        }
 }
