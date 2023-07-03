cf2b2c7924a47c4fe55576513f1185e35d75e4a0
shillahdan <awinosheila004@gmail.com>
2023-07-03 11:52:28 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
-int _putchar(char c)
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
